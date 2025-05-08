f09fb53083d8ca20f55025e632c3d719d9320d31
Chahak Jain <chahak.nikhil@gmail.com>
2025-05-09 00:08:27 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Welcome! This is a playlist generator</h1>
+      <p>We will be helping you curate your favorite playlists based off of your favorite artists, genres, and type of language you desire.</p>
+        {/*<img src={logo} className="App-logo" alt="logo" /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
